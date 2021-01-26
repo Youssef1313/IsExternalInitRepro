afc904afcf24ce5385cce0a89a28e21bb26e0a34
Youssef Victor <youssefvictor00@gmail.com>
2021-01-26 13:52:11 +0200

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int MyProperty { get; init; }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { } // Make it internal to fix the issue in the other project.
+}
---
+++ IsExternalInitRepro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IsExternalInitRepro
+{
+    class Program
+    {
+        public int MyProperty { get; init; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
